6291740a3c32e2d3f7dd51b821cf216bf6875b74
Er3nX0 <d8aniel8@outlook.com>
2025-01-30 13:28:31 +0100

    dodanie

--- app/src/main/java/com/example/mymultiactivityapp/MainActivity.java
+++ app/src/main/java/com/example/mymultiactivityapp/MainActivity.java
@@ -1,32 +1,57 @@
 package com.example.mymultiactivityapp;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button buttonNavigate;
+    private static final String CHANNEL_ID = "my_channel_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        buttonNavigate = findViewById(R.id.buttonNavigate);
+        createNotificationChannel();
+        Button notify = findViewById(R.id.notification);
+        notify.setOnClickListener(v -> sendNotification());
 
+    }
 
-        buttonNavigate.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-            intent.putExtra("message", "Witaj z MainActivity!");
-            startActivity(intent);
-        });
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Kanał Powiadomień";
+            String description = "Opis kanału powiadomień";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        }
+    }
+
+    private void sendNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        }
     }
 }
-
